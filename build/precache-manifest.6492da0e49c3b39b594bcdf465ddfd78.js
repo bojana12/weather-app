7b1f1966d4a37624ab3dbe254a8b486c39e54bcc
Bojana Karakacev <bojana.karakacev@gmail.com>
2020-01-13 22:44:31 +0100

    Build the app

---
+++ build/precache-manifest.6492da0e49c3b39b594bcdf465ddfd78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4380b4cf9db794cc3f8b495b522dfdaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df721a24eeffe1fcf5e5",
+    "url": "/static/css/main.8241ca47.chunk.css"
+  },
+  {
+    "revision": "3fcbf3d16bee785a0d58",
+    "url": "/static/js/2.2f8c5599.chunk.js"
+  },
+  {
+    "revision": "df721a24eeffe1fcf5e5",
+    "url": "/static/js/main.5f030fb7.chunk.js"
+  },
+  {
+    "revision": "52b73b35fadb36a52ff2",
+    "url": "/static/js/runtime-main.92ccb4ab.js"
+  },
+  {
+    "revision": "c64483b123a184f04ef1054fc9b34f4e",
+    "url": "/static/media/weather.c64483b1.jpg"
+  }
+]);
